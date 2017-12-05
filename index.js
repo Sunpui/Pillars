f5be04b9d22dc8611b6c56a56e2d472498dbc053
Binkyface <33262142+Binkyface@users.noreply.github.com>
2017-12-05 16:23:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const Command = require('command');
-module.exports = function Pillars(dispatch) {
+module.exports = function Pillars(dispatch) 
 	const command = Command(dispatch);
 	let enabled = false;
 	
-	dispatch.hook('S_SPAWN_WORKOBJECT', 1, (event) => {
-		if(enabled) {
+	dispatch.hook('S_SPAWN_WORKOBJECT', 1, event => {
+		if (enabled) {
 			dispatch.toServer('C_PREPARE_WORKOBJECT', 1, {
 				id:event.uid
-			})
+			});
 		}
 	});
 	
 	command.add('pillar', () => {
-		enabled = !enabled
-		command.message('Pillar module '+(enabled?'enabled.':'disabled.'))
-	)}
+		enabled = !enabled;
+		command.message('Pillar module '+(enabled?'enabled.':'disabled.'));
+	});
 }
